59d43d44965985b1903092748148ea1fa55e8a22
Mikael Lirbank <mikael@lirbank.com>
2025-09-06 10:18:38 -0700

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -27,7 +27,7 @@ function Home() {
   });
 
   return (
-    <div className="grid flex-1 grid-cols-2 gap-4 p-4">
+    <div className="grid flex-1 grid-cols-3 gap-4 p-4">
       <div className="flex flex-col gap-4 rounded bg-sky-100 p-4">
         RPC
         <button
@@ -83,7 +83,49 @@ function Home() {
                   });
                 }}
               >
-                {workspace.name} - {workspace.id}
+                {workspace.name} - {workspace.id.slice(0, 8)}
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="flex flex-col gap-4 rounded bg-amber-100 p-4">
+        Electric Live Query
+        <button
+          onClick={() => {
+            workspacesCollection.insert({
+              id: genSecureToken(),
+              name: "Workspace " + genSecureToken(3),
+              createdAt: new Date().toISOString(),
+              updatedAt: new Date().toISOString(),
+            });
+          }}
+          className="h-fit w-fit rounded bg-sky-500 px-4 py-1 text-white"
+        >
+          Create Workspace
+        </button>
+        <div>
+          {workspaces.data.map((workspace) => (
+            <div key={workspace.id} className="flex gap-2">
+              <div
+                role="button"
+                className="select-none"
+                onClick={() => {
+                  workspacesCollection.delete(workspace.id);
+                }}
+              >
+                Delete
+              </div>
+              <div
+                role="button"
+                className="select-none"
+                onClick={() => {
+                  workspacesCollection.update(workspace.id, (draft) => {
+                    draft.name = "Workspace 2 " + genSecureToken(3);
+                  });
+                }}
+              >
+                {workspace.name} - {workspace.id.slice(0, 8)}
               </div>
             </div>
           ))}
