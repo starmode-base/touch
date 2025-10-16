42dbe2f9cb50bb84b8fa30f10b37e93c9e2c5b3e
Mikael Lirbank <mikael@lirbank.com>
2025-10-16 07:41:17 -0700

    Update start.ts

--- src/start.ts
+++ src/start.ts
@@ -1,4 +1,8 @@
 /**
+ * https://github.com/clerk/clerk-docs/pull/2690/files
+ * https://github.com/clerk/clerk-docs/blob/rob/tanstack-start-rc/docs/getting-started/quickstart.tanstack-react-start.mdx
+ * https://github.com/clerk/clerk-docs/blob/rob/tanstack-start-rc/docs/reference/tanstack-react-start/clerk-middleware.mdx
+ *
  * https://github.com/clerk/javascript/blob/main/packages/tanstack-react-start/CHANGELOG.md
  * https://www.npmjs.com/package/@clerk/tanstack-react-start
  * https://clerk.com/docs/quickstarts/tanstack-react-start
