9cdfde1e8e2e9d7b1f80ecd5f1ed7293278a2532
Mikael Lirbank <mikael@lirbank.com>
2025-10-26 11:44:30 -0700

    Update api.contacts.ts

--- src/routes/api.contacts.ts
+++ src/routes/api.contacts.ts
@@ -1,20 +1,18 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { proxyElectricRequest } from "~/lib/electric";
 
-export const Route = createFileRoute("/api/contacts")({
-  server: {
-    handlers: {
-      GET: async ({ request }) => {
-        return proxyElectricRequest({
-          request,
-          table: "contacts",
-          where: (viewer) => {
-            return viewer.workspaceMembershipIds.length
-              ? `workspace_id IN (${viewer.workspaceMembershipIds.map((id) => `'${id}'`).join(",")})`
-              : `FALSE`;
-          },
-        });
-      },
+const serve = (args: { request: Request }) => {
+  return proxyElectricRequest({
+    request: args.request,
+    table: "contacts",
+    where: (viewer) => {
+      return viewer.workspaceMembershipIds.length
+        ? `workspace_id IN (${viewer.workspaceMembershipIds.map((id) => `'${id}'`).join(",")})`
+        : `FALSE`;
     },
-  },
+  });
+};
+
+export const Route = createFileRoute("/api/contacts")({
+  server: { handlers: { GET: serve } },
 });
