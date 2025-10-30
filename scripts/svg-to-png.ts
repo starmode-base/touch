360dac8858ef879184228c7096d322b08609ee29
Mikael Lirbank <mikael@lirbank.com>
2025-10-29 17:10:40 -0700

    Icons

--- scripts/svg-to-png.ts
+++ scripts/svg-to-png.ts
@@ -1,9 +1,14 @@
 #!/usr/bin/env bun
+import path from "path";
 import sharp from "sharp";
 
 export async function convertSvgToPng(inputFileUrl: URL, sizes: number[]) {
+  const baseName = path.basename(
+    inputFileUrl.pathname,
+    path.extname(inputFileUrl.pathname),
+  );
   for (const size of sizes) {
-    const filename = `icon-${size}.png`;
+    const filename = `${baseName}-${size}.png`;
     const outputUrl = new URL(filename, inputFileUrl);
 
     await sharp(inputFileUrl.pathname)
@@ -17,3 +22,7 @@ export async function convertSvgToPng(inputFileUrl: URL, sizes: number[]) {
 
 const sizes = [16, 32, 48, 64, 128, 256, 512];
 await convertSvgToPng(new URL("../public/icon.svg", import.meta.url), sizes);
+await convertSvgToPng(
+  new URL("../public/icon-gray.svg", import.meta.url),
+  sizes,
+);
