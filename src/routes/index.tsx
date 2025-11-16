c6ca81b43428cdcd1217c95ee704e647e1a9676a
Mikael Lirbank <mikael@lirbank.com>
2025-11-16 09:05:36 -0800

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,6 +2,6 @@ import { createFileRoute, redirect } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/")({
   beforeLoad: () => {
-    return redirect({ to: "/contacts" });
+    void redirect({ to: "/contacts" });
   },
 });
