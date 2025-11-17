1d73924774720aebfb7fc1220d576c8d58be0405
Mikael Lirbank <mikael@lirbank.com>
2025-11-16 16:01:06 -0800

    Update viewer.ts

--- src/server-functions/viewer.ts
+++ src/server-functions/viewer.ts
@@ -2,6 +2,5 @@ import { createServerFn } from "@tanstack/react-start";
 import { syncViewer } from "~/lib/auth";
 
 export const syncViewerSF = createServerFn().handler(() => {
-  console.log("Syncing viewer");
   return syncViewer();
 });
