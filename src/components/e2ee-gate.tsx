0ff4a7efa0a1a9dfaf34903cdfdfdc577c52acad
Mikael Lirbank <mikael@lirbank.com>
2025-11-19 11:04:01 -0800

    Update e2ee-gate.tsx

--- src/components/e2ee-gate.tsx
+++ src/components/e2ee-gate.tsx
@@ -1,7 +1,5 @@
 import { useE2ee } from "./hooks/e2ee";
 import { E2eeEnrollment } from "./e2ee-enrollment";
-import { E2eeUnlock } from "./e2ee-unlock";
-import { cryptoSession } from "~/lib/e2ee-session";
 
 export function E2eeGate(props: React.PropsWithChildren) {
   const { passkeys } = useE2ee();
@@ -10,10 +8,6 @@ export function E2eeGate(props: React.PropsWithChildren) {
 
   if (!hasPasskeys) {
     return <E2eeEnrollment />;
-  }
-
-  if (!cryptoSession.exists()) {
-    return <E2eeUnlock passkeys={passkeys} />;
   }
 
   return props.children;
