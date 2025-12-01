43fba6d629cebf1553380857d15487f9deebcba6
Mikael Lirbank <mikael@lirbank.com>
2025-12-01 00:38:33 -0800

    Update auth-middleware.ts

--- src/middleware/auth-middleware.ts
+++ src/middleware/auth-middleware.ts
@@ -2,13 +2,11 @@ import { createMiddleware } from "@tanstack/react-start";
 import { getViewer } from "~/lib/auth";
 
 /**
- * Middleware to ensure the viewer is signed in and has a viewer record in the
- * database.
+ * Middleware to ensure the viewer is signed in
  */
 export const ensureViewerMiddleware = createMiddleware({
   type: "function",
 }).server(async ({ next }) => {
-  // Get the current clerk user id
   const viewer = await getViewer();
 
   if (!viewer) {
