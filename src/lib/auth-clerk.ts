57fb7e50690f5ff71fda01ec3a5e7656cc57f49e
Mikael Lirbank <mikael@lirbank.com>
2025-11-30 20:52:43 -0800

    mv

--- src/lib/auth-clerk.ts
+++ src/lib/auth-clerk.ts
@@ -1,8 +1,54 @@
 import { eq, sql } from "drizzle-orm";
 import { db, schema } from "~/postgres/db";
 import { memoizeAsync } from "./memoize";
-import { auth } from "./auth";
 import { getRequestHeaders } from "@tanstack/react-start/server";
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { emailOTP } from "better-auth/plugins";
+
+export const auth = betterAuth({
+  database: drizzleAdapter(db(), {
+    provider: "pg",
+  }),
+  emailAndPassword: {
+    enabled: true,
+    autoSignIn: false,
+    requireEmailVerification: true,
+  },
+  emailVerification: {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    sendVerificationEmail: async ({ user, url, token }, request) => {
+      console.log("sendVerificationEmail⚡️", user);
+      console.log("sendVerificationEmail⚡️", url);
+      console.log("sendVerificationEmail⚡️", token);
+
+      // await sendEmail({
+      //   to: user.email,
+      //   subject: "Verify your email address",
+      //   text: `Click the link to verify your email: ${url}`,
+      // });
+
+      return Promise.resolve();
+    },
+  },
+  plugins: [
+    emailOTP({
+      async sendVerificationOTP({ email, otp, type }) {
+        if (type === "sign-in") {
+          console.log("sign-in⚡️", type, email, otp);
+          // Send the OTP for sign in
+        } else if (type === "email-verification") {
+          console.log("email-verification⚡️", type, email, otp);
+          // Send the OTP for email verification
+        } else {
+          console.log("forget-password⚡️", type, email, otp);
+          // Send the OTP for password reset
+        }
+        return Promise.resolve();
+      },
+    }),
+  ],
+});
 
 /**
  * Viewer type
