6f3718e5eec9f14e583fd4b382242eda2bf0511c
Mikael Lirbank <mikael@lirbank.com>
2025-11-30 21:17:28 -0800

    mv

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,11 +1,6 @@
 import { createAuthClient } from "better-auth/react";
 import { emailOTPClient } from "better-auth/client/plugins";
 
-// import metadata from "../../metadata.json";
-
 export const authClient = createAuthClient({
-  /** The base URL of the server (optional if you're using the same domain) */
-  //   baseURL: `http://localhost:${metadata.dev.port}`,
-
   plugins: [emailOTPClient()],
 });
