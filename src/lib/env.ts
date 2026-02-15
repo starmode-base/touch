811dcffda52d838dda874182a019f5d57544f4db
Mikael Lirbank <mikael@lirbank.com>
2026-02-15 12:06:19 -0800

    Envs

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -1,15 +1,16 @@
+/* eslint-disable @typescript-eslint/no-unnecessary-condition */
 import { env } from "cloudflare:workers";
 import { z } from "zod";
 
 export const ensureEnv = () => {
   console.log(
     "ensureEnv|process.env.DATABASE_URL",
-    typeof process.env.DATABASE_URL,
+    typeof process?.env?.DATABASE_URL,
   );
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   // @ts-ignore
 
-  console.log("ensureEnv|env.DATABASE_URL", typeof env.DATABASE_URL);
+  console.log("ensureEnv|env.DATABASE_URL", typeof env?.DATABASE_URL);
 
   return z
     .object({
