6f1b91799f8dce145116017205c58f3a0a46941a
Mikael Lirbank <mikael@lirbank.com>
2026-02-15 12:23:56 -0800

    Deps

--- src/routeTree.gen.ts
+++ src/routeTree.gen.ts
@@ -233,7 +233,7 @@ declare module '@tanstack/react-router' {
     '/_auth': {
       id: '/_auth'
       path: ''
-      fullPath: ''
+      fullPath: '/'
       preLoaderRoute: typeof AuthRouteImport
       parentRoute: typeof rootRouteImport
     }
