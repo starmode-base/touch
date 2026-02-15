7efe494d2c50ba20e10c14574486c4f9bd7f6b55
Mikael Lirbank <mikael@lirbank.com>
2026-02-15 12:05:01 -0800

    Envs

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,11 +3,11 @@ import { defineConfig } from "drizzle-kit";
 const url = process.env.DATABASE_URL;
 if (!url) throw new Error("DATABASE_URL is required for drizzle-kit");
 
-console.log("process.env.DATABASE_URL", process.env.DATABASE_URL);
+console.log("process.env.DATABASE_URL", typeof process.env.DATABASE_URL);
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-console.log("env.DATABASE_URL", env.DATABASE_URL);
+console.log("env.DATABASE_URL", typeof env.DATABASE_URL);
 
 export default defineConfig({
   out: "./src/postgres/migrations",
