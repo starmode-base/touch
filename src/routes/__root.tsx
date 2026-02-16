0f27052de8711ca16dea6fb065aa86145ffbf44d
Mikael Lirbank <mikael@lirbank.com>
2026-02-15 17:20:30 -0800

    Update __root.tsx

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,6 +1,5 @@
 import { HeadContent, Scripts, createRootRoute } from "@tanstack/react-router";
 import { TanStackRouterDevtoolsPanel } from "@tanstack/react-router-devtools";
-import { TanStackDevtools } from "@tanstack/react-devtools";
 
 import Header from "../components/Header";
 
@@ -39,17 +38,6 @@ function RootDocument({ children }: { children: React.ReactNode }) {
       <body>
         <Header />
         {children}
-        <TanStackDevtools
-          config={{
-            position: "bottom-right",
-          }}
-          plugins={[
-            {
-              name: "Tanstack Router",
-              render: <TanStackRouterDevtoolsPanel />,
-            },
-          ]}
-        />
         <Scripts />
       </body>
     </html>
